d5db804bf8224a941fdc4c9d2972eaec39f3734e
vikashagrahari43 <vikashagrahari43@gmail.com>
2025-08-03 20:44:20 +0530

    responsiveness 04

--- DailyBlog/src/pages/Login.jsx
+++ DailyBlog/src/pages/Login.jsx
@@ -3,7 +3,7 @@ import { Login as LoginComponent} from '../Component'
 
 function Login() {
   return (
-    <div className='md:py-8 py-2'>
+    <div className='md:py-8 py-2 m-0'>
         <LoginComponent />
     </div>
   )
